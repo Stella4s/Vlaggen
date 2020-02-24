044cf3864c7bc0a487b46e3bdcae0925baa08d70
Stella van der burg <estellavanderburg@gmail.com>
2020-02-24 15:27:48 +0100

    Found a way to access the images in resources and put them in a list. Put them all into a class list and then display one of the images visually along with it's name. (Lots of optimising to do, but the starting point is there.)

---
+++ ItViteaVlaggen01/FlagDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Drawing;
+using System.Resources;
+
+namespace ItViteaVlaggen01
+{
+    class FlagDetails
+    {
+        /// <summary>
+        /// The name of the country that will be used. (based of FileName)
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The actual filename. Flag[country].png
+        /// </summary>
+        public string FileName { get; set; }
+
+        public string ImgSource { get; set; }
+        /// <summary>
+        /// The image object.
+        /// </summary>
+        public object Img { get; set; }
+
+        public int ID { get; set; }
+
+        public void setNameAndImgSource()
+        {
+            Name = FileName.Replace("Flag", "");
+            ImgSource = String.Format(@"Resources\{0}.png", FileName);
+        }
+    }
+
+   
+}
+ 
--- ItViteaVlaggen01/MainWindow.xaml.cs
+++ ItViteaVlaggen01/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
 
 namespace ItViteaVlaggen01
 {
@@ -23,15 +28,36 @@ namespace ItViteaVlaggen01
         public MainWindow()
         {
             InitializeComponent();
+
+            ResourceSet rsrcSet = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, false, true);
+
+
+            var images = typeof(Properties.Resources)
+                    .GetProperties(BindingFlags.Static | BindingFlags.NonPublic |
+                                                         BindingFlags.Public)
+                    .Where(p => p.PropertyType == typeof(Bitmap))
+                    .Select(x => new { Name = x.Name, Image = x.GetValue(null, null) })
+                    .ToList();
+
+            List<FlagDetails> flagsList = new List<FlagDetails>();
+            int intID = 1;
+            foreach (var x in images)
+            {
+                flagsList.Add(new FlagDetails { FileName = x.Name, Img = x.Image, ID = intID});
+                intID++;
+            }
+            foreach (FlagDetails x in flagsList){
+                x.setNameAndImgSource();
+            }
+
+            BitmapImage bimage = new BitmapImage();
+            bimage.BeginInit();
+            bimage.UriSource = new Uri(flagsList[4].ImgSource, UriKind.Relative);
+            bimage.EndInit();
+            image1.Source = bimage;
+
+            textBlock1.Text = flagsList[4].Name;
         }
-        public void InitializeImageList()
-        {
-           //Flags flag1 = new Flags { Number = 1, Name = "Nepal", Image = image1.Source; }
 
-        }
-
-
-
-        
     }
 }
